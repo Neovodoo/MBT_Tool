0e9ac7f395632cad859826c3553077015013935d
Neovodoo <75006634+Neovodoo@users.noreply.github.com>
2025-07-27 22:11:31 +0300

    Add a simple extraction tool for test case data from openAPI scheme

--- MBT_tool_new/test_generator/cli.py
+++ MBT_tool_new/test_generator/cli.py
@@ -1,6 +1,12 @@
-from generator.parser import load_yaml
+from generator.parser import load_yaml, extract_paths
 
 if __name__ == "__main__":
     data = load_yaml('api.yaml')
-    print(data)
+    endpoints = extract_paths(data)
 
+    print("Найденные эндпоинты:\n")
+    for endpoint in endpoints:
+        print(f"Путь: {endpoint['path']}")
+        print(f"Метод: {endpoint['method']}")
+        print(f"Описание: {endpoint['summary']}")
+        print('-' * 20)
--- MBT_tool_new/test_generator/generator/parser.py
+++ MBT_tool_new/test_generator/generator/parser.py
@@ -4,3 +4,19 @@ def load_yaml(file_path: str) -> dict:
     with open(file_path, 'r', encoding='utf-8') as f:
         yaml_data = yaml.safe_load(f)
     return yaml_data
+
+
+def extract_paths(yaml_data: dict) -> list:
+    paths = yaml_data.get('paths', {})
+    endpoints = []
+
+    for path, methods in paths.items():
+        for method, details in methods.items():
+            summary = details.get('summary', '')
+            endpoint_info = {
+                'path': path,
+                'method': method.upper(),
+                'summary': summary
+            }
+            endpoints.append(endpoint_info)
+    return endpoints
