a31cb8f07aa240bf9af67fd5014a76499c30fbd7
Neovodoo <75006634+Neovodoo@users.noreply.github.com>
2025-07-27 20:30:53 +0300

    Remove old version of MBT tool

--- autotest_generation/utils/Constants.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-
-GENERATED_DIR = Path("generated_tests")
--- autotest_generation/utils/TestCaseReader.py
+++
@@ -1,5 +0,0 @@
-import yaml
-
-def load_test_cases(yaml_path: str) -> dict:
-    with open(yaml_path, "r", encoding="utf-8") as f:
-        return yaml.safe_load(f)
