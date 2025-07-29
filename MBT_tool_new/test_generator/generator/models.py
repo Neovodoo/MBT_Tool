0f12ddc972c787e2a1c756d579bd12d97601284a
Neovodoo <75006634+Neovodoo@users.noreply.github.com>
2025-07-29 22:06:28 +0300

    Add parameters for test cases

--- MBT_tool_new/test_generator/generator/models.py
+++ MBT_tool_new/test_generator/generator/models.py
@@ -1,10 +1,22 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Optional, List
+
+
+#TODO: Избавиться от влияния порядка объявления классов на их параметры
+@dataclass
+class Parameter:
+    name: str
+    in_: str  # слово in – ключевое в Python, поэтому используем in_
+    required: bool
+    type: str
+    example: Optional[str] = None
 
 @dataclass
 class Endpoint:
     path: str
     method: str
     summary: str
+    parameters: List[Parameter] = field(default_factory=list)
 
 @dataclass
 class TestCase:
@@ -13,9 +25,24 @@ class TestCase:
     description: str
 
     def to_text(self) -> str:
+        # TODO: Вынести формирование текста для параметров в отдельный метод
+        params_text = ""
+        if self.endpoint.parameters:
+            params_text = "Параметры запроса:\n"
+            for param in self.endpoint.parameters:
+                params_text += (
+                    f" - {param.name} ({param.in_}), "
+                    f"тип: {param.type}, "
+                    f"{'обязательный' if param.required else 'необязательный'}\n"
+                )
+        else:
+            params_text = "Параметры: отсутствуют\n"
+
+
         return (
             f"Тест-кейс: {self.name}\n"
             f"Метод: {self.endpoint.method}\n"
             f"Путь: {self.endpoint.path}\n"
             f"Описание: {self.description}\n"
+            f"{params_text}"
         )
--- MBT_tool_new/test_generator/generator/parser.py
+++ MBT_tool_new/test_generator/generator/parser.py
@@ -1,5 +1,5 @@
 import yaml
-from .models import Endpoint
+from .models import Endpoint, Parameter
 
 def load_yaml(file_path: str) -> dict:
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -13,12 +13,28 @@ def extract_paths(yaml_data: dict) -> list:
 
     for path, methods in paths.items():
         for method, details in methods.items():
+
             summary = details.get('summary', '')
+
+            # Извлекаем параметры
+            params_data = details.get('parameters', [])
+            parameters = []
+
+            for p in params_data:
+                param = Parameter(
+                    name=p['name'],
+                    in_=p['in'],
+                    required=p.get('required', False),
+                    type=p['schema']['type'],
+                    example=None  #TODO: Доработать позже
+                )
+                parameters.append(param)
 
             endpoint = Endpoint(
                 path=path,
                 method=method.upper(),
-                summary=summary
+                summary=summary,
+                parameters=parameters
             )
 
             endpoints.append(endpoint)
