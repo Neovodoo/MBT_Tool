859b7c539acba4eeaf6faf6b5a1db50bfbb9c334
Neovodoo <75006634+Neovodoo@users.noreply.github.com>
2025-09-16 22:18:45 +0300

    Clear project

--- MBT_tool_new/test_generator/generator/generator.py
+++
@@ -1,5 +0,0 @@
-def save_test_cases_to_file(test_cases: list, filename: str):
-    with open(filename, 'w', encoding='utf-8') as f:
-        for test_case in test_cases:
-            f.write(test_case.to_text())
-            f.write('\n' + '-' * 50 + '\n\n')
